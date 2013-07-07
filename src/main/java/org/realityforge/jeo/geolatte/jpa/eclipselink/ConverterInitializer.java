905e7685c4652840cb902ee8c6707bcb31a55bbe
Peter Donald <peter@realityforge.org>
2013-07-07 13:18:22 +1000

    Initial code drop

---
+++ src/main/java/org/realityforge/jeo/geolatte/jpa/eclipselink/ConverterInitializer.java
@@ -0,0 +1,93 @@
+package org.realityforge.jeo.geolatte.jpa.eclipselink;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Vector;
+import org.eclipse.persistence.descriptors.ClassDescriptor;
+import org.eclipse.persistence.mappings.DatabaseMapping;
+import org.eclipse.persistence.mappings.DirectToFieldMapping;
+import org.eclipse.persistence.mappings.converters.Converter;
+import org.eclipse.persistence.mappings.converters.SerializedObjectConverter;
+import org.eclipse.persistence.sessions.Session;
+import org.eclipse.persistence.sessions.SessionEvent;
+import org.eclipse.persistence.sessions.SessionEventAdapter;
+import org.geolatte.geom.Geometry;
+import org.geolatte.geom.LineString;
+import org.geolatte.geom.LinearRing;
+import org.geolatte.geom.MultiLineString;
+import org.geolatte.geom.MultiPoint;
+import org.geolatte.geom.MultiPolygon;
+import org.geolatte.geom.Point;
+import org.geolatte.geom.Polygon;
+
+public class ConverterInitializer
+  extends SessionEventAdapter
+{
+  @Override
+  public void preLogin( final SessionEvent event )
+  {
+    final Session session = event.getSession();
+    final Map<Class, ClassDescriptor> descriptorMap = session.getDescriptors();
+
+    // Walk through all descriptors...
+    for ( final Map.Entry<Class, ClassDescriptor> entry : descriptorMap.entrySet() )
+    {
+      final ClassDescriptor desc = entry.getValue();
+      final Vector<DatabaseMapping> mappings = desc.getMappings();
+      // walk through all mappings for some class...
+      for ( final DatabaseMapping mapping : mappings )
+      {
+        if ( mapping instanceof DirectToFieldMapping )
+        {
+          final DirectToFieldMapping dfm = (DirectToFieldMapping) mapping;
+          if ( isCandidateConverter( dfm ) )
+          {
+            final Class type = entry.getKey();
+            final String attributeName = dfm.getAttributeName();
+            final Field field = getField( type, attributeName );
+            final Class<?> fieldType = field.getType();
+            if ( Point.class == fieldType ||
+                 Polygon.class == fieldType ||
+                 LineString.class == fieldType ||
+                 MultiPoint.class == fieldType ||
+                 MultiPolygon.class == fieldType ||
+                 MultiLineString.class == fieldType ||
+                 Geometry.class == fieldType ||
+                 LinearRing.class == fieldType )
+            {
+              final Converter converter = new PostgisConverter();
+              converter.initialize( mapping, session );
+              dfm.setConverter( converter );
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private Field getField( final Class type, final String attributeName )
+  {
+    Class t = type;
+    while ( t != Object.class )
+    {
+      try
+      {
+        return t.getDeclaredField( attributeName );
+      }
+      catch ( final NoSuchFieldException nsfe )
+      {
+        //Ignore
+      }
+      t = t.getSuperclass();
+    }
+    throw new IllegalStateException( "Unable to find field: " + attributeName );
+  }
+
+  // only consider mappings that are deemed to produce
+  // byte[] database fields from objects...
+  private boolean isCandidateConverter( final DirectToFieldMapping mapping )
+  {
+    final Converter converter = mapping.getConverter();
+    return null != converter && converter instanceof SerializedObjectConverter;
+  }
+}
---
+++ src/main/java/org/realityforge/jeo/geolatte/jpa/eclipselink/PostgisConverter.java
@@ -0,0 +1,141 @@
+package org.realityforge.jeo.geolatte.jpa.eclipselink;
+
+import java.lang.reflect.Field;
+import java.sql.SQLException;
+import org.eclipse.persistence.internal.helper.DatabaseField;
+import org.eclipse.persistence.mappings.DatabaseMapping;
+import org.eclipse.persistence.mappings.DirectCollectionMapping;
+import org.eclipse.persistence.mappings.converters.Converter;
+import org.eclipse.persistence.sessions.Session;
+import org.geolatte.geom.Geometry;
+import org.geolatte.geom.LineString;
+import org.geolatte.geom.LinearRing;
+import org.geolatte.geom.MultiLineString;
+import org.geolatte.geom.MultiPoint;
+import org.geolatte.geom.MultiPolygon;
+import org.geolatte.geom.Point;
+import org.geolatte.geom.Polygon;
+import org.geolatte.geom.codec.Wkt;
+import org.geolatte.geom.codec.Wkt.Dialect;
+import org.postgis.PGgeometry;
+
+public class PostgisConverter
+  implements Converter
+{
+  @Override
+  public Object convertObjectValueToDataValue( final Object objectValue, final Session session )
+  {
+    if( null == objectValue )
+    {
+      return null;
+    }
+    final Geometry geometry = (Geometry) objectValue;
+    final String wkt = Wkt.newEncoder( Dialect.POSTGIS_EWKT_1 ).encode( geometry );
+    try
+    {
+      return new PGgeometry( wkt );
+    }
+    catch ( SQLException e )
+    {
+      throw new IllegalStateException( "Failed converting geometry", e );
+    }
+  }
+
+  @Override
+  public Geometry convertDataValueToObjectValue( final Object dataValue, final Session session )
+  {
+    if( null == dataValue )
+    {
+      return null;
+    }
+    final String wkt;
+    if ( dataValue instanceof PGgeometry )
+    {
+      final org.postgis.Geometry geometry = ( (PGgeometry) dataValue ).getGeometry();
+      wkt = geometry.toString();
+    }
+    else
+    {
+      wkt = (String) dataValue;
+    }
+    return Wkt.newDecoder( Dialect.POSTGIS_EWKT_1 ).decode( wkt );
+  }
+
+  @Override
+  public boolean isMutable()
+  {
+    return false;
+  }
+
+  @Override
+  public void initialize( final DatabaseMapping mapping, final Session session )
+  {
+    final DatabaseField field;
+    if ( mapping instanceof DirectCollectionMapping )
+    {
+      field = ( (DirectCollectionMapping) mapping ).getDirectField();
+    }
+    else
+    {
+      field = mapping.getField();
+    }
+    field.setSqlType( java.sql.Types.OTHER );
+    if ( null == field.getTypeName() )
+    {
+      field.setTypeName( "geometry" );
+    }
+    if ( null == field.getColumnDefinition() )
+    {
+      final Field javaField = getJavaField( mapping, field );
+      final Class<?> javaFieldType = javaField.getType();
+      //TODO: Dervive the SRS from an annotation
+      //TODO: Dervive the M from an annotation
+      if ( Point.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(POINT,-1)" );
+      }
+      else if ( Polygon.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(POLYGON,-1)" );
+      }
+      else if ( LineString.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(LINESTRING,-1)" );
+      }
+      else if ( MultiPoint.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(MULTIPOINT,-1)" );
+      }
+      else if ( MultiPolygon.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(MULTIPOLYGON,-1)" );
+      }
+      else if ( MultiLineString.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(MULTILINESTRING,-1)" );
+      }
+      else if ( LinearRing.class == javaFieldType )
+      {
+        field.setColumnDefinition( "geometry(LINEARRING,-1)" );
+      }
+      else
+      {
+        field.setColumnDefinition( "geometry" );
+      }
+    }
+  }
+
+  private Field getJavaField( final DatabaseMapping mapping, final DatabaseField field )
+  {
+    try
+    {
+      final String fieldName = field.getName();
+      final Class type = mapping.getDescriptor().getJavaClass();
+      return type.getField( fieldName );
+    }
+    catch ( final NoSuchFieldException nsfe )
+    {
+      throw new IllegalStateException( "Unable to locate expected field", nsfe );
+    }
+  }
+}
